ea48e6e2fd893002de720310c328a728841023ea
İsmail Emre Canpınar <emrecanpinar06@gmail.com>
2024-02-28 13:24:57 +0300

    move main to WireMaster

--- src/WireMaster.h
+++ src/WireMaster.h
@@ -1,13 +1,43 @@
-#include "imgui_node_editor.h"
-#include "imgui.h"
+# include <imgui.h>
+# include <imgui_node_editor.h>
+# include <application.h>
 
 
-class WireMaster
+namespace ed = ax::NodeEditor;
+
+struct WireMaster : public Application
 {
-private:
-    /* data */
-public:
     void draw();
+
+
+    // Struct to hold basic information about connection between
+    // pins. Note that connection (aka. link) has its own ID.
+    // This is useful later with dealing with selections, deletion
+    // or other operations.
+    struct LinkInfo
+    {
+        ed::LinkId Id;
+        ed::PinId  InputId;
+        ed::PinId  OutputId;
+    };
+
+    using Application::Application;
+
+    void OnStart() override;
+    void OnStop() override;
+
+    void ImGuiEx_BeginColumn();
+
+    void ImGuiEx_NextColumn();
+
+    void ImGuiEx_EndColumn();
+
+    void OnFrame(float deltaTime) override;
+
+    ed::EditorContext*   m_Context = nullptr;    // Editor context, required to trace a editor state.
+    bool                 m_FirstFrame = true;    // Flag set for first frame only, some action need to be executed once.
+    ImVector<LinkInfo>   m_Links;                // List of live links. It is dynamic unless you want to create read-only view over nodes.
+    int                  m_NextLinkId = 100;     // Counter to help generate link ids. In real application this will probably based on pointer to user data structure.
 };
 
 
